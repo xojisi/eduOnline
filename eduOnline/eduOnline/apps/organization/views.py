35f5eaeb1de75fbdb4f4d83782eb2337d3f3df3a
lipeiyu <271398289@qq.com>
2017-12-25 23:55:44 +0800

    add the function:Course Organization management (unfinished 12.25 23:55)

--- eduOnline/eduOnline/apps/organization/views.py
+++ eduOnline/eduOnline/apps/organization/views.py
@@ -12,7 +12,7 @@ class OrgView(View):
     def get(self,request):
         # 课程机构
         all_orgs = CourseOrg.objects.all()
-
+        hot_orgs = all_orgs.order_by("-click_num")[:3]
         # 城市
         all_citys = CityDict.objects.all()
 
@@ -25,6 +25,13 @@ class OrgView(View):
         category = request.GET.get('ct', "")
         if category:
             all_orgs = all_orgs.filter(category=category)
+
+        sort = request.GET.get("sort",'')
+        if sort:
+            if sort == "students":
+                all_orgs = all_orgs.order_by("-students")
+            elif sort == "courses":
+                all_orgs = all_orgs.order_by("-course_nums")
 
         org_nums = all_orgs.count()
 
@@ -42,5 +49,7 @@ class OrgView(View):
             "all_citys": all_citys,
             "org_nums": org_nums,
             "city_id": city_id,
-            "category": category
+            "category": category,
+            "hot_orgs": hot_orgs,
+            "sort": sort
         })
